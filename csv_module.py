914911ef68aec02321ac9163a25ff3a40e28a1ef
jeong <jeong@gafs.co.jp>
2025-05-16 09:39:55 +0900

    refactor: 変数の名前をより直観的に変更

--- csv_module.py
+++ csv_module.py
@@ -2,8 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 
-
-class FileTypeError(Exception):
+class InvalidFileTypeError(Exception):
     """CSV파일이 아닐 경우"""
     pass
 
@@ -12,73 +11,80 @@ class CSVFileReadError(Exception):
     pass
 
 
-def csv_file_load(file_path, delimiter=',', how = 'np'):
+def load_csv_file(csv_path, delimiter=',', loader = 'np'):
     """
     CSV파일을 읽어온다.
-    :param file_path: CSV파일 경로
+    :param csv_path: CSV파일 경로
     :param delimiter: CSV파일 구분자
-    :param how: np, pd
+    :param loader: np, pd
     :return: CSV파일 데이터
     """
     
-    def csv_file_load_np(file_path, delimiter=','):
+    def _load_csv_with_numpy(csv_path, delimiter=','):
         """
         numpy로 CSV파일을 읽어온다.
-        :param file_path: CSV파일 경로
+        :param csv_path: CSV파일 경로
         :param delimiter: CSV파일 구분자
         :return: CSV파일 데이터
         """
         data = None
         try:    
-            data = np.genfromtxt(file_path, delimiter=delimiter, skip_header=True, filling_values=0)
+            data = np.genfromtxt(csv_path, delimiter=delimiter, skip_header=True, filling_values=0)
         except Exception as e:
             print(e)
         return data
 
-    def csv_file_load_pd(file_path, delimiter=','):
+    def _load_csv_with_pandas(csv_path, delimiter=','):
         """
         pandas로 CSV파일을 읽어온다.
-        :param file_path: CSV파일 경로
+        :param csv_path: CSV파일 경로
         :param delimiter: CSV파일 구분자
         :return: CSV파일 데이터
         """
         data = None
         try:    
-            data = pd.read_csv(file_path, delimiter=delimiter)
+            data = pd.read_csv(csv_path, delimiter=delimiter)
         except Exception as e:
             print(e)
         return data
 
-    if path_error_check(file_path):
-        pass
+    csv_path = validate_csv_path(csv_path)
 
-    if how.startswith('np') or how.endswith('np'):
+    if loader.startswith('np') or loader.endswith('np'):
         try:
-            data = csv_file_load_np(file_path, delimiter)
+            data = _load_csv_with_numpy(csv_path, delimiter)
         except Exception as e:
             print(e)
             print("load error for pandas")
         
 
-    if how.startswith('pd') or how.endswith('pd'):
+    if loader.startswith('pd') or loader.endswith('pd'):
         try:
-            data = csv_file_load_pd(file_path)
+            data = _load_csv_with_pandas(csv_path)
         except Exception as e:
             print(e)
             print("load error for pandas")
 
     if data is None:
-        raise CSVFileReadError("Error reading CSV file: {}".format(file_path))
+        raise CSVFileReadError("Error reading CSV file: {}".format(csv_path))
     
     return data
 
 
-def path_error_check(file_path):
+def validate_csv_path(csv_path):
     """
     파일의 경로를 체크한다
     """
-    if not file_path.endswith('.csv'):
-        raise FileTypeError("File must be a CSV file.")
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"File not found: {file_path}")
-    return file_path
+    if not csv_path.endswith('.csv'):
+        raise InvalidFileTypeError("File must be a CSV file.")
+    if not os.path.exists(csv_path):
+        raise FileNotFoundError(f"File not found: {csv_path}")
+    return csv_path
+
+if __name__ == "__main__":
+    # Example usage
+    example_file_path = '../sample_waves.csv'
+    example_file_path = validate_csv_path(example_file_path)
+    data = load_csv_file(example_file_path, delimiter=',', loader='np')
+
+    print(data)
