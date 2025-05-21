c09fa56de1f9c0c3e737968e4d394f470f940a23
jeong <jeong@gafs.co.jp>
2025-05-21 13:57:45 +0900

    fix: excute_cli.py欠落した実行設定の追加 - fillna - fillna_value - fmt - image_name(save_graph_name)

--- execute_cli.py
+++ execute_cli.py
@@ -31,6 +31,29 @@ if __name__ == "__main__":
                         action="store_true", 
                         help="グラフをファイルとして保存するかどうか"
                         )
+    
+    parser.add_argument("-f","--fillna",
+                        default=True,
+                        action="store_false", 
+                        help="設定時、NaNを'-fv'で埋めしない"
+                        )
+    
+    parser.add_argument("-fv","--fillna_value",
+                        default=0, 
+                        type=float,
+                        help="NaNを埋める値（デフォルト: 0）"
+                        )
+    
+    parser.add_argument("-fmt","--fmt",
+                        default='%8g', 
+                        help="CSV出力時の数値フォーマット（デフォルト: %8g）。\
+                            有効数字8桁、指数表記対応、末尾の不要なゼロは自動的に省略されます。"
+                        )
+    
+    parser.add_argument("-img","--save_graph_name",
+                        default=None, 
+                        help="グラフの保存名（デフォルト: None）"
+                        )
 
     args = parser.parse_args()
 
@@ -40,4 +63,8 @@ if __name__ == "__main__":
         save_path=args.save_path,
         new_data_name=args.new_data_name,
         save_graph=args.save_graph,
+        fillna=args.fillna,
+        fillna_value=args.fillna_value,
+        fmt=args.fmt,
+        image_name=args.save_graph_name
     )
