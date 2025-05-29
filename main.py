0291cd6f6a23570517d343d1108a69ee70c31654
jeong <jeong@gafs.co.jp>
2025-05-29 15:49:39 +0900

    fix: 修正にあわせ実行コードの修正

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ def main(
         new_data_name = "synthetic wave", 
         save_path = './added_data.csv', 
         save_graph = False,
-        fillna = True,
-        fillna_value = 0,
+        # fillna = True,
+        # fillna_value = 0,
         fmt = '%8g',
         image_name = None):
     
-    summer = CSVColumnSummer(csv_file_path, delimiter=delimiter,fillna=fillna,fillna_value=fillna_value)
+    # summer = CSVColumnSummer(csv_file_path,{'delimiter':delimiter,'fillna':fillna,'fillna_value':fillna_value, 'fmt':fmt})
+    summer = CSVColumnSummer(csv_file_path,{'delimiter':delimiter, 'fmt':fmt})
     x_data, y_data = summer.get_data()
-    summer.save_combined(header = new_data_name, save_path = save_path)
-    summer.set_config(fmt=fmt)
+    summer.save_data(header = new_data_name, save_path = save_path)
+    summer.set_options(fmt=fmt)
 
     plotter = Plotter()
     plotter.set_plot(x_data,y_data,labels={-1: new_data_name})
-
+    
     if save_graph:
         save_graph_name = save_path.split('.csv')[0] if image_name is None else image_name
         plotter.save_plot(save_graph_name)
